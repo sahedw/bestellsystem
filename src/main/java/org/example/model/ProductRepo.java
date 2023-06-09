714d42b975744aee9194d432bab67847b74bb6c2
Enrico <e.weissenrieder94@gmail.com>
2023-06-09 11:14:00 +0200

    added getProduct functionality

--- src/main/java/org/example/model/ProductRepo.java
+++ src/main/java/org/example/model/ProductRepo.java
@@ -1,4 +1,20 @@
 package org.example.model;
 
-public class ProductRepo {
+import java.util.List;
+import java.util.Map;
+
+public class ProductRepo{
+
+    Map<String, String> products;
+    public ProductRepo(Map<String, String> products){
+        this.products = products;
+    }
+
+    public Map<String, String> list(){
+        return this.products;
+    };
+
+    public String get(String id){
+      return products.get(id);
+    };
 }
