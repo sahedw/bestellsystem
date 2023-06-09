d998a0e0721a7a86ef11d7add15993c8f193e95c
Enrico <e.weissenrieder94@gmail.com>
2023-06-09 11:15:50 +0200

    added Product class

---
+++ src/main/java/org/example/model/Product.java
@@ -0,0 +1,11 @@
+package org.example.model;
+
+public class Product {
+    public Product(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String id;
+    public String name;
+}
